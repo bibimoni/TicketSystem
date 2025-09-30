6fa47fc16726fbbe75ee995351690e530d674653
bibimoni <proquadi66@gmail.com>
2025-09-30 17:10:15 +0700

    init backend (again)

---
+++ ticket-system-backend/src/database/database.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI!, {
+      dbName: process.env.MONGO_DB,
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule { }
+

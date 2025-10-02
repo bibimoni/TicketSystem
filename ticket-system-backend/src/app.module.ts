3e867b9a5ef26afa71d5352305d481174a368dd6
bibimoni <proquadi66@gmail.com>
2025-10-02 18:37:46 +0700

    added customer route, added prisma, configure docker. Added readme on how to use it.

--- ticket-system-backend/src/app.module.ts
+++ ticket-system-backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CustomerModule } from './customer/customer.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { CustomerController } from './customer/customer.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    CustomerModule,
+    PrismaModule
+  ],
+  controllers: [AppController, CustomerController],
+  providers: [PrismaService, AppService],
+  exports: [PrismaService]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ ticket-system-backend/src/customer/dto/create-customer.dto.ts
@@ -0,0 +1,5 @@
+export class CreateCustomerDto {
+  email: string
+  username: string
+  password: string
+}

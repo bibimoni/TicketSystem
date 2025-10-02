005cd8704564a17b9eb21e7cdbfd0ac294b6b4a3
bibimoni <proquadi66@gmail.com>
2025-10-03 02:44:34 +0700

    Fix: Added `config.load()` in `main.ts`

--- ticket-system-backend/src/main.ts
+++ ticket-system-backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { config } from 'src/config/config';
 
 async function bootstrap() {
+  config.load();
   const app = await NestFactory.create(AppModule);
   await app.listen(config.port);
 }

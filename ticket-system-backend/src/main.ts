051aaa87744c968bfab2022935d948c7a9f1395f
bibimoni <proquadi66@gmail.com>
2025-10-03 17:09:46 +0700

    working: added swagger

--- ticket-system-backend/src/main.ts
+++ ticket-system-backend/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'src/config/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   config.load();
   const app = await NestFactory.create(AppModule);
+
+  const swaggerCfg = new DocumentBuilder()
+    .setTitle('Ticket System')
+    .setDescription('This documentation will contain API description for all the available api')
+    .setVersion('1.0')
+    .addBearerAuth({
+
+    })
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerCfg)
+  SwaggerModule.setup('api', app, documentFactory)
+
   await app.listen(config.port);
 }
 bootstrap();

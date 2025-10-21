488281041cc05927f11f9aff0f06190644949317
bibimoni <proquadi66@gmail.com>
2025-10-22 00:09:44 +0700

    added ticket

--- ticket-system-backend/src/auth/auth.service.ts
+++ ticket-system-backend/src/auth/auth.service.ts
@@ -11,7 +11,6 @@ export class AuthService {
 
   async signIn(username: string, password: string): Promise<any> {
     const user = await this.prisma.user.findUnique({ where: { username } })
-    this.logger.log("hi");
     if (!user) {
       throw new UnauthorizedException();
     }

cb7760e666e4ff6e5f51cbda85f49c8fb23a03a7
bibimoni <proquadi66@gmail.com>
2025-10-23 02:51:33 +0700

    finished event creation, but i haven't included admin verification yet
    Cherry-pick from bao-backend Event Module

--- ticket-system-backend/src/event/event.controller.ts
+++ ticket-system-backend/src/event/event.controller.ts
@@ -1,24 +1,99 @@
-import { Controller, Get, Post, Body, Param, Request, UseGuards, HttpStatus, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Request, UseGuards, HttpStatus, HttpCode, UnauthorizedException, BadRequestException } from '@nestjs/common';
 import { EventService } from './event.service';
 import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
 import { PublicEventResponseDto } from './dto/public-event-response';
 import { ApiBearerAuth, ApiBody, ApiHeader, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateEventCustomerDto } from './dto/create-event-customer.dto';
+import { TicketService } from 'src/ticket/ticket.service';
 
 @Controller('event')
 export class EventController {
-  constructor(private readonly eventService: EventService) { }
+  constructor(
+    private readonly eventService: EventService,
+    private readonly ticketService: TicketService
+  ) { }
 
+  @ApiOperation({ summary: 'Get all events' })
   @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: 'Event create' })
-  @ApiBody({ type: CreateEventDto })
-  @ApiResponse({
-    description: "Successfully create event",
-    type: PublicEventResponseDto
-  })
-  @Post()
-  async create(@Body() createEventDto: CreateEventDto): Promise<PublicEventResponseDto> {
-    return await this.eventService.create(createEventDto);
+  @Get('/all')
+  async findAllEvents() {
+    return await this.eventService.findAllEvents();
+  }
+
+  // @HttpCode(HttpStatus.OK)
+  // @ApiOperation({ summary: 'Event create' })
+  // @ApiBody({ type: CreateEventDto })
+  // @ApiResponse({
+  //   description: "Successfully create event",
+  //   type: PublicEventResponseDto
+  // })
+  // @Post()
+  // async create(@Body() createEventDto: CreateEventDto): Promise<PublicEventResponseDto> {
+  //   return await this.eventService.create(createEventDto);
+  // }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Create Event by Customer' })
+  @ApiBearerAuth('JWT-auth')
+  @Post('create')
+  async createEvent(@Body() createEventCustomerDto: CreateEventCustomerDto, @Request() req: any) {
+    const username = req.user.username;
+    if (!username) {
+      throw new UnauthorizedException()
+    }
+
+    const createdEvent = await this.eventService.createEventByCustomer(createEventCustomerDto, username);
+
+    await Promise.all(
+      createEventCustomerDto.ticketsType.map(async ticketType => {
+        const createdTicketPrice = await this.ticketService.createTicketPrice({
+          price: ticketType.price,
+          name: ticketType.name,
+          benefit_info: ticketType.benefit_info
+        })
+
+        if (!createdTicketPrice) {
+          throw new BadRequestException("Failed to create ticket price")
+        }
+
+        const tickets = await Promise.all(
+          ticketType.tickets.map(async ticketDto => {
+            const createdTicket = await this.ticketService.createTicket(
+              ticketDto,
+              createdEvent.id,
+              createdTicketPrice.id
+            )
+            if (!createdTicket) {
+              throw new BadRequestException("Failed to create ticket")
+            }
+            return createdTicket;
+          })
+        )
+
+        if (!tickets) {
+          throw new BadRequestException("Failed to create tickets")
+        }
+        return createdTicketPrice;
+      })
+    )
+
+    return { event_id: createdEvent.id };
+  }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Customer Get All Events' })
+  @ApiBearerAuth('JWT-auth')
+  @Get('customer_events')
+  async findAllByCustomer(@Request() req: any) {
+    const username = req.user.username;
+    if (!username) {
+      throw new UnauthorizedException()
+    }
+    return await this.eventService.findAllByCustomer(username);
   }
 
   // Action of admin: view all event

dc777a39253fc87adcbd16977b9a7b2d83f6bac3
unknown <phuong.hoanghcmut106@hcmut.edu.vn>
2025-12-14 19:44:30 +0700

    Move frontend source into TicketSystem-hotfix-thanh-frontend folder

--- src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-// src/lib/utils.js
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Hàm gộp className an toàn khi dùng Tailwind + clsx
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
